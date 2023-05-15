88faf4be400482066b3ccc410e4c348f31307bb7
Diogo Manuel Peixoto Maia <diogompmaia@gmail.com>
2023-05-15 17:04:24 +0100

    Added rgb to hex conversion

--- src/auxiliary/helperMethods.js
+++ src/auxiliary/helperMethods.js
@@ -1,15 +1,5 @@
 function getColors(){
-
-    const number = Math.floor(Math.random() * 256)
-    return number 
-    
-}
-
-function rgbToHex() {
-    const componentToHex = (c) => {
-      const hex = c.toString(16);
-      return hex.length === 1 ? "0" + hex : hex;
-    };
+ 
   
     const red = componentToHex(getColors());
     const green = componentToHex(getColors());
